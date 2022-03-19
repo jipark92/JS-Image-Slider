fa0ad71b69b0cfd5b9e93d89c72b86399ee84ea0
ji <jiyongprk@gmail.com>
2022-03-19 01:04:13 -0400

    start with querySelectors

---
+++ main.js
@@ -0,0 +1,5 @@
+const prevBtns = document.querySelector('.prev-btn');
+const nextBtns = document.querySelector('.next-btn')
+const pictureContainer = document.querySelector('.picture-container');
+const pictureFrame = document.querySelector('.picture-frame');
+const images = document.querySelector('img')
